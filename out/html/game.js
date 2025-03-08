7c129ec2995346c3c20333dc604cb7f5dbc5410e
meneerpluis <nathangroenheide@gmail.com>
2025-03-08 01:04:08 +0100

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "The Second Republic: An Alternate History" + '_' + "meneerpluis";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
